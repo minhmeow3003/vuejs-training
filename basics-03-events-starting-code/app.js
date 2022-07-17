6859224bd07f295f7e28084f88838e3b3bcfc338
Minh <lelun999@gmail.com>
2022-07-17 20:22:07 +0700

    learning event

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,8 +2,27 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
+      confirmedName: "",
     };
+  },
+  methods: {
+    comfirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      alert("Submitted!");
+    },
+    setName(e, lastName) {
+      this.name = e.target.value + " " + lastName;
+    },
+    increase(num) {
+      this.counter = this.counter + num;
+    },
+    decrease(num) {
+      this.counter = this.counter - num;
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
