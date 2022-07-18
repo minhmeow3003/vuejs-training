d49a807cfbbd04220a1e79f8cb19302d621d5cd8
Minh <lelun999@gmail.com>
2022-07-19 00:27:56 +0700

    add folder about native event

---
+++ basics-05-using-the-native-event-object/app.js
@@ -0,0 +1,66 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: "",
+      lastName: "",
+      // fullname: "",
+    };
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(() => {
+          that.counter = 0;
+        }, 2000);
+      }
+    },
+    //   name(value) {
+    //     if (value === "") {
+    //       this.fullname = "";
+    //     } else {
+    //       this.fullname = value + " " + this.lastName;
+    //     }
+    //   },
+    //   lastName(value) {
+    //     if (value === "") {
+    //       this.fullname = "";
+    //     } else {
+    //       this.fullname = this.name + " " + value;
+    //     }
+    //   },
+  },
+  computed: {
+    fullname() {
+      console.log("Running...");
+      if (this.name === "" || this.lastName === "") {
+        return "";
+      }
+      return this.name + " " + this.lastName;
+    },
+  },
+  methods: {
+    outputFullname() {
+      if (this.name === "") {
+        return "";
+      }
+      return this.name + " " + "Minh";
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput() {
+      this.name = "";
+    },
+  },
+});
+
+app.mount("#events");
