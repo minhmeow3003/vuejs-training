8210cd41e4ddf74a1c6bad52e430a442e5212688
Minh <lelun999@gmail.com>
2022-07-27 23:07:35 +0700

    finish section9

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <!-- <TheHeader/> -->
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <!-- <active-goals v-if="selectComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectComponent === 'manage-goals'"></manage-goals> -->
+    <keep-alive>
+      <component :is="selectComponent"></component>
+    </keep-alive>
+    <!-- <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+    <course-goals #default="slotProps">
+      <h2>{{ slotProps.item }}</h2>
+      <p>{{ slotProps["another-prop"] }}</p>
+    </course-goals> -->
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/layout/TheHeader.vue";
+// import BadgeList from "./components/BadgeList.vue";
+// import UserInfo from "./components/UserInfo.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
+
+export default {
+  components: {
+    TheHeader,
+    // BadgeList,
+    // UserInfo,
+    // CourseGoals,
+    ActiveGoals,
+    ManageGoals,
+  },
+  data() {
+    return {
+      selectComponent: "active-goals",
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectComponent = cmp;
+    },
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
